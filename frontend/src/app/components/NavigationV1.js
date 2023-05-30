6c9317bffb064d1d93355d96543f3203440283ab
sarwarg <mdbscc321@gmail.com>
2023-05-30 12:27:57 +0530

    remove unuse state

--- frontend/src/app/components/NavigationV1.js
+++ frontend/src/app/components/NavigationV1.js
@@ -1,47 +1,48 @@
-import React, { useContext, useEffect, useState, useRef } from 'react'
-import Filter from './Filter'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngleLeft } from '@fortawesome/free-solid-svg-icons'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
-import { Link, NavLink } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import Filter from './Filter';
 
 const NavigationV1 = () => {
-    const [backgroundColors,setbackgroundColor] = useState(false);
+  const [backgroundColors, setbackgroundColor] = useState(false);
 
-const changeBackground = () =>{
-  if(window.scrollY >=30){
-    setbackgroundColor(true);
-  }else{
-    setbackgroundColor(false);
-  }
-}
-useEffect(()=>{
-    changeBackground()
-    window.addEventListener("scroll",changeBackground);
-},[])
-    return (
-       
-      <div className="filter-nav">
-      <div className="row">
-      <div className="col-lg-3"></div>
-      <div className="col-lg-9">
-      <div className={backgroundColors?'filter-section active':'filter-section'}>
-                <div className="back-section">
-                  <ol className="breadcrumb">
-                    {/* <li className="breadcrumb-item"><Link to="/">
+  const changeBackground = () => {
+    if (window.scrollY >= 30) {
+      setbackgroundColor(true);
+    } else {
+      setbackgroundColor(false);
+    }
+  };
+  useEffect(() => {
+    changeBackground();
+    window.addEventListener('scroll', changeBackground);
+  }, []);
+  return (
+    <div className='filter-nav'>
+      <div className='row'>
+        <div className='col-lg-3'></div>
+        <div className='col-lg-9'>
+          <div
+            className={
+              backgroundColors ? 'filter-section active' : 'filter-section'
+            }
+          >
+            <div className='back-section'>
+              <ol className='breadcrumb'>
+                {/* <li className="breadcrumb-item"><Link to="/">
                       <FontAwesomeIcon icon={faAngleLeft} /> Back
                     </Link></li> */}
-                    <li className="breadcrumb-item active" aria-current="page">Champions</li>
-                  </ol>
-                </div>
-                <div className="search-section">
-                  <Filter />
-                </div>
-              </div>
+                <li className='breadcrumb-item active' aria-current='page'>
+                  Champions
+                </li>
+              </ol>
+            </div>
+            <div className='search-section'>
+              <Filter />
+            </div>
+          </div>
+        </div>
       </div>
-      </div>
-      </div>
-    )
-}
+    </div>
+  );
+};
 
-export default NavigationV1
+export default NavigationV1;
--- frontend/src/app/components/Pagination.js
+++ frontend/src/app/components/Pagination.js
@@ -1,24 +1,36 @@
-import React,{useState} from 'react'
-import  Pagination  from 'react-bootstrap/Pagination'
+import React from 'react';
+import Pagination from 'react-bootstrap/Pagination';
 
-const PaginationComponent = ({championPerPage,totalChampions,paginate,currentPage,handleNext,handlePrev}) => {
- 
-  const pages = Math.ceil(totalChampions/championPerPage);
-   const pageNumber = [];
-   for(let i=1;i<=pages;i++){
-       pageNumber.push(i)
-   }
+const PaginationComponent = ({
+  championPerPage,
+  totalChampions,
+  paginate,
+  currentPage,
+  handleNext,
+  handlePrev,
+}) => {
+  const pages = Math.ceil(totalChampions / championPerPage);
+  const pageNumber = [];
+  for (let i = 1; i <= pages; i++) {
+    pageNumber.push(i);
+  }
 
-    return (
-        <Pagination>
-          <Pagination.Prev onClick={()=>handlePrev(pageNumber)}/>
-          {pageNumber.map(number=>(
-            <Pagination.Item key={number} className={currentPage===number && "active"} onClick={()=>paginate(number)}>{number}</Pagination.Item>
-          ))}
-           
-          <Pagination.Next onClick={()=>handleNext(pageNumber)}/>
-        </Pagination>
-    )
-}
+  return (
+    <Pagination>
+      <Pagination.Prev onClick={() => handlePrev(pageNumber)} />
+      {pageNumber.map((number) => (
+        <Pagination.Item
+          key={number}
+          className={currentPage === number && 'active'}
+          onClick={() => paginate(number)}
+        >
+          {number}
+        </Pagination.Item>
+      ))}
 
-export default PaginationComponent
+      <Pagination.Next onClick={() => handleNext(pageNumber)} />
+    </Pagination>
+  );
+};
+
+export default PaginationComponent;
